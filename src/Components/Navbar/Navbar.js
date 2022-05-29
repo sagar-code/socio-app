7b2d9f0400868ee331816930c13949ecd2488216
sagar-code <sagarfmt@gmail.com>
2022-05-29 11:20:13 +0545

    wip logout

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
+
+import { AuthConsumer } from "../../Contexts/AuthContext";
 import Logo from "../../Assets/Logos/socio.png";
 import Signup from "../../Assets/Icons/singup.png";
 
 const Navbar = () => {
+  const { currentUser } = AuthConsumer();
+  console.log(currentUser);
   return (
     <div className="bg-white shadow">
       <div className="container mx-auto py-3 flex justify-between items-center">
@@ -10,14 +14,24 @@ const Navbar = () => {
           <img src={Logo} className="h-12" alt="socio-logo" />
         </div>
         <div>
-          <Link
-            to="/signup"
-            type="text"
-            className="border-2 rounded-md border-primary px-4 py-2 text-primary hover:border-orange-400"
-          >
-            <img src={Signup} alt="signup-icon" className="inline h-4 pr-2" />
-            Sign In
-          </Link>
+          {currentUser.email ? (
+            <Link
+              to="/profile"
+              type="text"
+              className="border-2 rounded-md border-primary px-4 py-2 text-primary hover:border-orange-400"
+            >
+              Profile
+            </Link>
+          ) : (
+            <Link
+              to="/signup"
+              type="text"
+              className="border-2 rounded-md border-primary px-4 py-2 text-primary hover:border-orange-400"
+            >
+              <img src={Signup} alt="signup-icon" className="inline h-4 pr-2" />
+              Sign In
+            </Link>
+          )}
         </div>
       </div>
     </div>
